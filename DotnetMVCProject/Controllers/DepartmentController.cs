63a16978e2abac823a1a04e126fe2fc1f31eea85
Ahmed Naser <ahmednaser7897@gmail.com>
2026-09-14 17:36:43 +0300

    add lec 2

---
+++ DotnetMVCProject/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using DotnetMVCProject.Models.Data;
+using DotnetMVCProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetMVCProject.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        readonly AppDbContext context = new();
+
+        // http://localhost:5074/Department/Index
+        public IActionResult Index()
+        {
+            List<Department> departments = context.Departments.Include(d => d.Employees).ToList();
+            return View("Index", departments);
+        }
+    }
+}
---
+++ DotnetMVCProject/Controllers/EmployeeController.cs
@@ -0,0 +1,103 @@
+﻿using DotnetMVCProject.Models;
+using DotnetMVCProject.Models.Data;
+using DotnetMVCProject.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetMVCProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // we can pass data to the view with 2 ways
+        // 1- by sending it to View() method 
+        // and take it from Model in the view
+        // 2- using ViewData dictionary
+        // we add the key in ViewData and take it from Model in the view
+        // we can add any type of data to the ViewData dictionary
+        // we can also pass the Model itself using ViewData.Model 
+        // 3- using ViewBag dynamic property
+        // we add the key in ViewBag and take it from Model in the view
+        // we can add any type of data to the ViewBag dynamic property
+        // we can also pass the Model itself using ViewBag.Model 
+        // it enable us to not using casting in the view
+        //----------------------------------------------
+        // 4- using ViewModel 
+        // we create a new class that contain the data we want to pass to the view
+        // and we pass it to the View() method 
+        // and we use it in the view
+        readonly AppDbContext context = new();
+        // http://localhost:5074/Employee/ViewDataDetails?id=1
+        public IActionResult ViewDataDetails(int id)
+        {
+            Employee? employee = context.Employees.Find(id);
+            if (employee != null)
+            {
+                string? msg = $"This {employee!.Name} data";
+                const int temp = 33;
+                List<string> branches = ["Cairo", "Alex", "Aswan"];
+                ViewData["msg"] = msg;
+                ViewData["temp"] = temp;
+                ViewData["branches"] = branches;
+                ViewData.Model = employee;
+                return View("ViewDataDetails", employee);
+            }
+            else
+            {
+                return NotFound($"Employee with id {id} is not found");
+            }
+
+
+        }
+        // http://localhost:5074/Employee/ViewBagDetails?id=1
+        public IActionResult ViewBagDetails(int id)
+        {
+            Employee? employee = context.Employees.Find(id);
+            if (employee != null)
+            {
+                string? msg = $"This {employee!.Name} data";
+                const int temp = 33;
+                List<string> branches = ["Cairo", "Alex", "Aswan"];
+                ViewBag.msg = msg;
+                ViewBag.temp = temp;
+                ViewBag.branches = branches;
+                ViewBag.Model = employee;
+                return View("ViewBagDetails", employee);
+            }
+            else
+            {
+                return NotFound($"Employee with id {id} is not found");
+            }
+
+
+        }
+
+        // http://localhost:5074/Employee/ViewModelDetails?id=1
+        public IActionResult ViewModelDetails(int id)
+        {
+            Employee? employee = context.Employees
+            .Include(e => e.Department)
+            .FirstOrDefault(e => e.Id == id);
+            if (employee != null)
+            {
+                EmployeeDataViewModel viewModel = new()
+                {
+                    Employee = employee,
+                    Message = "This {employee!.Name} data",
+                    Temperature = 33,
+                    Branches = ["Cairo", "Alex", "Aswan"],
+                    Color = "red",
+                    DeptName = employee?.Department?.Name ?? "Unknown"
+                };
+
+                return View("ViewModelDetails", viewModel);
+            }
+            else
+            {
+                return NotFound($"Employee with id {id} is not found");
+            }
+
+
+        }
+
+    }
+}
